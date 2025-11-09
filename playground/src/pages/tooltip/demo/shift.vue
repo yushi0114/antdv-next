cffc6d7e4b2022cccff47784fc6a48bff994b57f
aibayanyu20 <aibayanyu@qq.com>
2025-11-09 15:20:55 +0800

    chore: change

---
+++ playground/src/pages/tooltip/demo/shift.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  document.documentElement.scrollTop = document.documentElement.clientHeight
+  document.documentElement.scrollLeft = document.documentElement.clientWidth
+})
+</script>
+
+<template>
+  <div class="h-300vh w-300vw flex items-center justify-center">
+    <a-tooltip title="Thanks for using antd. Have a nice day !" open arrow>
+      <a-button type="primary">
+        Scroll The Window
+      </a-button>
+    </a-tooltip>
+  </div>
+</template>
