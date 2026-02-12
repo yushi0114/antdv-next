92558085a85c1494b50954a5621035c0b9ef1fee
aibayanyu20 <aibayanyu@qq.com>
2026-02-12 11:59:42 +0800

    chore: update test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,6 +11,10 @@ export default defineConfig({
         'packages/antdv-next/src/**/*.{ts,tsx}',
         'packages/cssinjs/src/**/*.{ts,tsx}',
       ],
+      exclude: [
+        'packages/**/locale/*.{ts,tsx}',
+        'packages/antdv-next/src/components.ts',
+      ],
     },
   },
 })
